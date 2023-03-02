a8594510b2ab31aa33a1c41593d703496384439f
cwainright-nps <charles_wainright@nps.gov>
2023-03-02 16:11:18 -0500

    cleaned up workflow to reflect current status of method testing

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from collections import OrderedDict # https://realpython.com/python-ordereddict/#:~:text=Python's%20OrderedDict%20is%20a%20dict,then%20the%20order%20remains%20unchanged.
 import xmltodict
-import dicttoxml
+from dicttoxml import dicttoxml
 import emld
 import importlib
+from xml.dom.minidom import parseString
 importlib.reload(emld)
 
 '''
 A mock workflow to test how py_emleditor
 '''
+# instantiate an emld object
 myfile = 'C:/Users/cwainright/OneDrive - DOI/Documents/data_projects/2023/20230210_iss135_emleditor/sandbox/2022_NCRN_forest_vegetation_metadata.xml'
 myemld = emld.Emld(filepath = myfile)
 
 
-myemld.set_cui(eml_object=myemld, cui_code="abc", force=True, NPS=True)
+# test `set_CUI()` (controlled unclassified information) attribute and value
+myemld.set_cui(cui_code="PUBLIC", force=True)
+print(xmltodict.unparse(myemld.emld["additionalMetadata"], pretty=True)) # test that the method worked
 
-mytestoutput = dicttoxml.dicttoxml(myemld)
 
+# test `set_title()` set dataset title value
+myemld.emld["dataset"]["title"]
+myemld.set_title(data_package_title = "my new title")
+myemld.emld["dataset"]["title"]
+
+# print the eml to console
+myemld.print_eml()
+
+# write the eml to file
+myemld.write_eml(destination_filename="sandbox/mytestoutput2.xml")
 
